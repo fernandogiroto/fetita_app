cd19babde5d148043e78bf60bc525b75910a214b
fernandogiroto <fernando.giroto@binalogue.com>
2023-09-22 08:45:57 -0300

    favicon add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Fetita') }}</title>
+        <link rel="icon" type="image/x-icon" href="../js/Assets/images/favicon.png">
+        <link rel="icon" href="{{url('/images/favicon.png')}}" type="image/x-icon"/>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
